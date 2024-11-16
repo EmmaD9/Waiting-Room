66a87823fa92e6c008f19645b2ac2737879675a4
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2024-11-16 15:45:17 -0500

    basic dialogue system in

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public Text nameText;
+    public Text dialogueText;
 
     private Queue<string> sentences;     //uses FIFO (first in first out) collection
     
@@ -11,12 +15,41 @@ public class DialogueManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         sentences = new Queue<string>();
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
+        //Debug.Log("Starting conversation with " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
     }
 
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        //Debug.Log(sentence);
+
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
 }
