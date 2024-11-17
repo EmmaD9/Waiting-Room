bd4084b9dd04fc4f237f7b2a9ac66d924e6d67cc
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2024-11-16 22:07:33 -0500

    red dialogue box works!!!!!

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -10,4 +10,18 @@ public class DialogueTrigger : MonoBehaviour
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+
+    public void TestDebug()
+    {
+        Debug.Log("Test");
+    }
+
+    void OnMouseDown()
+    { 
+        // This method is called when the user clicks on the GameObject
+        Debug.Log("GameObject clicked!");
+        TriggerDialogue();
+        // Trigger your custom event here 
+        // e.g., Call a method or change a property
+    }
 }
